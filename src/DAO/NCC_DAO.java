40af0697a3cd0f14683e6708d5a359c698959598
hoanglap_Jay <hoanglap0206@gmail.com>
2026-01-22 21:19:58 +0700

    Update NhanVien,NhaCungCap - DAO

--- src/DAO/NCC_DAO.java
+++ src/DAO/NCC_DAO.java
@@ -1,4 +1,81 @@
 package DAO;
 
+import DataBase.DBConnection;
+import Model.NhaCungCap;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class NCC_DAO {
+    public ArrayList<NhaCungCap> getAllNCC(){
+        ArrayList<NhaCungCap> list = new ArrayList<>();
+        String sql = "SELECT * FROM NHA_CUNG_CAP";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+        ) {
+            while(rs.next()){
+                NhaCungCap ncc = new NhaCungCap();
+                ncc.setMaNCC(rs.getString("ma_ncc"));
+                ncc.setTenNCC(rs.getString("ten_ncc"));
+                ncc.setDiaChi(rs.getString("dia_chi"));
+                ncc.setSdt(rs.getString("sdt"));
+                list.add(ncc);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    public boolean insert(NhaCungCap ncc){
+        String sql ="INSERT INTO NHA_CUNG_CAP(ma_ncc,ten_ncc,dia_chi,sdt) VALUES (?, ?, ?,?)";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)
+        ){
+            ps.setString(1,ncc.getMaNCC());
+            ps.setString(2,ncc.getTenNCC());
+            ps.setString(3,ncc.getDiaChi());
+            ps.setString(4,ncc.getSdt());
+            int rows = ps.executeUpdate();
+            return rows > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean update(NhaCungCap ncc){
+        String sql =
+                "UPDATE NHA_CUNG_CAP SET ten_ncc=?,dia_chi=?,sdt=? WHERE ma_ncc=?";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)
+        ){
+            ps.setString(1,ncc.getTenNCC());
+            ps.setString(2,ncc.getDiaChi());
+            ps.setString(3,ncc.getSdt());
+            ps.setString(4,ncc.getMaNCC());
+            int rows = ps.executeUpdate();
+            return rows > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean delete(String maNCC){
+        String sql = "DELETE FROM NHA_CUNG_CAP WHERE ma_ncc=?";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
+            ps.setString(1, maNCC);
+            return ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- src/DAO/NV_DAO.java
+++ src/DAO/NV_DAO.java
@@ -1,4 +1,87 @@
 package DAO;
 
+import DataBase.DBConnection;
+import Model.NhanVien;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class NV_DAO {
+    public ArrayList<NhanVien> getAllNV(){
+        ArrayList<NhanVien> list = new ArrayList<>();
+        String sql = "SELECT * FROM NHAN_VIEN";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+        ) {
+            while(rs.next()){
+                NhanVien nv = new NhanVien();
+                nv.setMaNV(rs.getString("ma_nhan_vien"));
+                nv.setHoTen(rs.getString("ho_ten"));
+                nv.setMatKhau(rs.getString("mat_khau"));
+                nv.setChucVu(rs.getString("chuc_vu"));
+                nv.setTrangThai(rs.getString("trang_thai"));
+                list.add(nv);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    public boolean insert(NhanVien nv){
+        String sql ="INSERT INTO NHAN_VIEN(ma_nhan_vien,ho_ten,mat_khau,chuc_vu,trang_thai) VALUES (?, ?, ?,?,?)";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
+            ps.setString(1,nv.getMaNV());
+            ps.setString(2,nv.getHoTen());
+            ps.setString(3,nv.getMatKhau());
+            ps.setString(4,nv.getChucVu());
+            ps.setString(5,nv.getTrangThai());
+            int rows = ps.executeUpdate();
+            return rows > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean update(NhanVien nv){
+//        Hàm này dựa vào id của nhân viên để sửa, bên xử lý sự kiện cần tạo ra nhân viên và lưu
+//  các thuộc tính,bao gồm ma_nhan_vien khi click vào dòng chứa nhân viên đó (MouseListener)
+//  lưu ý: không nên sửa đổi mã nhân viên. hàm xóa cũng tương tự
+        String sql =
+                "UPDATE NHAN_VIEN SET ho_ten=?,mat_khau=?,chuc_vu=?,trang_thai=? WHERE ma_nhan_vien=?";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
+            ps.setString(1,nv.getHoTen());
+            ps.setString(2,nv.getMatKhau());
+            ps.setString(3,nv.getChucVu());
+            ps.setString(4,nv.getTrangThai());
+            ps.setString(5,nv.getMaNV());
+            int rows = ps.executeUpdate();
+            return rows > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean delete(String maNv){
+        String sql = "DELETE FROM NHAN_VIEN WHERE ma_nhan_vien=?";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
+            ps.setString(1, maNv);
+            return ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
