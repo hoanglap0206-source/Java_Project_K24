1d97f4f7cf11049498686736026c3434ecfdd702
hoanglap_Jay <hoanglap0206@gmail.com>
2026-01-22 09:21:03 +0700

    Git_Tutorial

---
+++ src/DataBase/TestConnection.java
@@ -0,0 +1,9 @@
+package DataBase;
+
+import java.sql.Connection;
+import DataBase.DBConnection;
+public class TestConnection {
+    public static void main(String[] args) {
+        Connection conn = DBConnection.getConnection();
+    }
+}
