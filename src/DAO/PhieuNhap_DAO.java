1af0f8489d2abbabf8e3c1bdd9490267f6a8898f
hoanglap_Jay <hoanglap0206@gmail.com>
2026-01-29 10:02:55 +0700

    Update PhieuNhap

--- src/DAO/PhieuNhap_DAO.java
+++ src/DAO/PhieuNhap_DAO.java
@@ -1,4 +1,41 @@
 package DAO;
 
+import DataBase.DBConnection;
+import Model.NhaCungCap;
+import Model.NhanVien;
+import Model.PhieuNhap;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class PhieuNhap_DAO {
+    public ArrayList<PhieuNhap> getAllPhieuNhap(){
+        ArrayList<PhieuNhap> list = new ArrayList<>();
+        String sql = "SELECT * FROM PHIEU_NHAP";
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+        ) {
+            while(rs.next()){
+                PhieuNhap pn =new PhieuNhap();
+                NhanVien nv =new NhanVien();
+                NhaCungCap ncc =new NhaCungCap();
+                pn.setMaPN(rs.getString("ma_pn"));
+                pn.setNgay_ct(
+                        rs.getTimestamp("ngay_ct").toLocalDateTime()
+                );
+                ncc.setMaNCC(rs.getString("ma_ncc"));
+                pn.setNhaCC(ncc);
+                nv.setMaNV(rs.getString("ma_nhan_vien"));
+                pn.setNhanVien(nv);
+                list.add(pn);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
